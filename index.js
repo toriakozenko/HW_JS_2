f11c1fab02c92b8dc0923c351637f0cce5a4cca4
Viktoriia <toriakozenko@gmail.com>
2023-03-06 17:08:32 +0200

    added homework

---
+++ index.js
@@ -0,0 +1,59 @@
+// Assign: evaluation
+// var a = 5;
+// console.log("a>>>", a); 
+// var b, c;
+// console.log("b>>>", b, "c>>>", c);
+// b = ((a) * 5);
+// console.log("b>>>", b);
+// b = ((c) = b/2); 
+// console.log("b>>>", b);
+// console.log("c>>>", c);
+
+// Number: age
+// const currentYear = 2023;
+// const ageOfUser = prompt("How old are you?");
+// const yearOfBirth = currentYear - ageOfUser;
+// alert(`Your year of birth is ${yearOfBirth}`);
+
+// Number: temperature
+// const celsius = prompt("Enter the temperature in Celsius");
+// const toFahrenheit = (celsius * 1.8) + 32;
+// alert(`${celsius} degrees Celsius are equal ${toFahrenheit.toFixed(2)} degrees Fahrenheit.`);
+// const fahrenheit = prompt("Enter the temperature in Fahrenheit:");
+// const toCelsius = (fahrenheit - 32) / 1.8;
+// alert(`${fahrenheit} degrees Fahrenheit are equal ${toCelsius.toFixed(2)} degrees Celsius.`);
+
+// Number: divide
+// const divideNumber = prompt("Enter your number:");
+// const divisorNumber = prompt("Enter divisor number:");
+// const result = Math.floor(divideNumber / divisorNumber);
+// alert(`Your number is ${result}`);
+
+// Number: currency
+// const rate = 36.50;
+// const amount = prompt("How much do you want to change?");
+// const exchangeAmount = (amount * rate).toFixed(2);
+// alert(`Your currency exchange result is ${exchangeAmount} UAH`);
+
+// Number: RGB
+// const red = prompt("Enter the value of red in the decimal system:");
+// const green = prompt("Enter the value of green in the decimal system:");
+// const blue = prompt("Enter the value of blue in the decimal system:");
+// const hexRed = parseInt(red).toString(16);
+// const hexGreen = parseInt(green).toString(16);
+// const hexBlue = parseInt(blue).toString(16);
+// const color = `#${hexRed}${hexRed}${hexGreen}${hexGreen}${hexBlue}${hexBlue}`;
+// alert(`Your HEX color is ${color}`);
+
+// Number: flats
+// const floors = parseInt(prompt("Enter the number of floors: "));
+// const flatsPerFloor = parseInt(prompt("Enter the number of apartments per floor: "));
+// const flatNumber = parseInt(prompt("Enter the apartment number: "));
+// const entranceNumber = Math.ceil(flatNumber / (floors * flatsPerFloor));
+// const floorNumber = Math.ceil((flatNumber % (floors * flatsPerFloor)) / flatsPerFloor);
+// alert(`Flat ${flatNumber} is in the entrance ${entranceNumber}, on the floor ${floorNumber}.`);
+
+
+
+
+
